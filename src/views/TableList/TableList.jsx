44866f00ae240fdf9e714576ff7e9333d78f6d16
yassir ben abdallah <yassirbnabdllh@gmail.com>
2019-03-08 15:24:34 +0100

    second commit

--- src/views/TableList/TableList.jsx
+++ src/views/TableList/TableList.jsx
@@ -8,60 +8,40 @@ import {
   Row,
   Col
 } from "reactstrap";
-
+import axios from "axios";
 import { thead, tbody } from "variables/general";
 
 class RegularTables extends React.Component {
+  constructor(prop,contexte){
+    super(prop,contexte);
+    this.state={
+      users:[]
+    }
+
+    this.getAll();
+  }
+  getAll() {
+
+    axios.get("http://localhost:3002/users/all").then(item => {
+
+
+      console.log(item.data.data);
+
+      this.setState({users: item.data.data})
+    })
+
+  }
+
+
   render() {
     return (
       <div className="content">
         <Row>
           <Col xs={12}>
-            <Card>
-              <CardHeader>
-                <CardTitle tag="h4">Simple Table</CardTitle>
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead className="text-primary">
-                    <tr>
-                      {thead.map((prop, key) => {
-                        if (key === thead.length - 1)
-                          return (
-                            <th key={key} className="text-right">
-                              {prop}
-                            </th>
-                          );
-                        return <th key={key}>{prop}</th>;
-                      })}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {tbody.map((prop, key) => {
-                      return (
-                        <tr key={key}>
-                          {prop.data.map((prop, key) => {
-                            if (key === thead.length - 1)
-                              return (
-                                <td key={key} className="text-right">
-                                  {prop}
-                                </td>
-                              );
-                            return <td key={key}>{prop}</td>;
-                          })}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col xs={12}>
             <Card className="card-plain">
               <CardHeader>
-                <CardTitle tag="h4">Table on Plain Background</CardTitle>
-                <p className="card-category"> Here is a subtitle for this table</p>
+                <CardTitle tag="h4"></CardTitle>
+                <p className="card-category">Users</p>
               </CardHeader>
               <CardBody>
                 <Table responsive>
@@ -79,21 +59,30 @@ class RegularTables extends React.Component {
                     </tr>
                   </thead>
                   <tbody>
-                    {tbody.map((prop, key) => {
-                      return (
-                        <tr key={key}>
-                          {prop.data.map((prop, key) => {
-                            if (key === thead.length - 1)
-                              return (
-                                <td key={key} className="text-right">
-                                  {prop}
-                                </td>
-                              );
-                            return <td key={key}>{prop}</td>;
-                          })}
+                  {
+
+                    this.state.users.map(prop =>
+
+                        <tr>
+                          <td className="text-right">
+                            {prop.firstName}
+                          </td>
+
+                          <td className="text-right">
+                            {prop.lastName}
+                          </td>
+
+                          <td className="text-right">
+                            {prop.email}
+                          </td>
+                          <td className="text-right">
+                            {prop.societe}
+                          </td>
                         </tr>
-                      );
-                    })}
+                    )
+
+                  }
+
                   </tbody>
                 </Table>
               </CardBody>
